405322c2d233a0c5fdf289a123a814d1db8e7841
Alexandra Dernova <alexder@gmx.net>
2023-12-05 15:37:46 +0100

    Add: Add and Delete Methoden

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,16 +1,40 @@
-import java.util.Arrays;
+//import java.util.Arrays;
+import java.util.List;
 
 public class Library {
-    private Book[] books;
+   // private Book[] books;
+    private List<Book> books;
 
-    public Library(Book[] books) {
+    public Library(List<Book> books) {
         this.books = books;
     }
+    public void addBook(Book book){
+        this.books.add(book);
+    }
+    public void addBook(Book book, int index){
+        this.books.add( index, book);
+    }
+    public void deleteBook(Book book){
+        this.books.remove(book);
+    }
+    public void deleteBook(int index){
+        this.books.remove(index);
+    }
 
+    /* public Library(Book[] books) {
+            this.books = books;
+        }*/
     @Override
+    public String toString() {
+        return "Library{" +
+                "books=" + books +
+                '}';
+    }
+   /* @Override
     public String toString() {
         return "Library{" +
                 "books=" + Arrays.toString(books) +
                 '}';
-    }
+    }*/
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Book book1 = new Book("Book1", "Author1", "isbn1");
         Book book2 = new Book("Book2", "Author2", "isbn2");
         Book book3 = new Book("Book3", "Author3", "isbn3");
-        Book[] bookList = {book1, book2, book3};
+        Book book4 = new Book("Book4", "Author3", "isbn4");
+        List<Book> bookList = new ArrayList<>(Arrays.asList(book1, book2, book3));
+       /* oder
+        bookList.add(book1);
+        bookList.add(book2);
+        bookList.add(book3);*/
         Library library = new Library(bookList);
         System.out.println(library);
+        library.addBook(book4, 1);
+        System.out.println(library);
+        library.deleteBook(3);
+        System.out.println(library);
+
     }
 }
